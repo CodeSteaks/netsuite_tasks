be2082a2a66af14b5992a1295376903509874387
CodeSteaks <mianmujtaba89@gmail.com>
2024-03-04 01:08:51 +0500

    NS: Creating a new record on a ue

---
+++ src/FileCabinet/SuiteScripts/ues_create_phone_record.js
@@ -0,0 +1,32 @@
+/**
+ * @NApiVersion 2.1
+ * @ScriptType UserEventScript
+ */
+
+
+define(['N/record'], function (record) {
+
+    function afterSubmit(context) {
+        var employee = context.newRecord;
+        var empCode = employee.getValue("custentity_sdr_employee_code");
+        var supervisorId = employee.getValue("supervisor");
+        log.debug(empCode);
+        log.debug(supervisorId);
+    
+        if (context.type == context.UserEventType.CREATE) {
+            var phonCallRecord = record.create({
+                type : record.Type.PHONE_CALL,
+                defaultValues : {
+                    customform : -150
+                }
+            });
+            phonCallRecord.setValue("title", "Call HR for Benefits");
+            phonCallRecord.setValue("assigned", employee.id);
+            phonCallRecord.save();
+        }
+    }
+
+    return {
+        afterSubmit : afterSubmit
+    }
+});
