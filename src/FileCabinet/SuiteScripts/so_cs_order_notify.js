390aa538685c4a39869658d750b49ce231bd4cd0
CodeSteaks <mianmujtaba89@gmail.com>
2024-03-05 01:18:04 +0500

    NS: Task Practice module 5

---
+++ src/FileCabinet/SuiteScripts/so_cs_order_notify.js
@@ -0,0 +1,31 @@
+/**
+ * @NApiVersion 2.1
+ * @ScriptType ClientScript
+ */
+
+
+define(['N/ui/dialog', 'N/email'], function (dialog, email) {
+
+    function pageInit (context) {
+        var order = context.currentRecord;
+        var status = order.getText("orderstatus");
+
+        if (status == "Billed" && context.mode == "edit") {
+            dialog.alert({
+                title   : "Edit Warning",
+                message : "This order has already been billed. Auditors will be notified of this action."
+            });
+
+            email.send({
+                author     : -5,
+                recipients : -5,
+                subject    : "User has edited a billed order",
+                message    : "Order " + order.getValue("tanid") + " has been recently opened."
+            });
+        }
+    }
+
+    return {
+        pageInit : pageInit
+    }
+});
