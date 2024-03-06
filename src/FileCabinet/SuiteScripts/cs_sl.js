0021781e010a190026fd7857192f39d170e05643
CodeSteaks <mianmujtaba89@gmail.com>
2024-03-06 09:30:23 +0500

    Sublist Scripting

---
+++ src/FileCabinet/SuiteScripts/cs_sl.js
@@ -0,0 +1,28 @@
+/**
+ * @NApiVersion 2.1
+ * @ScriptType ClientScript
+ */
+
+define(['N/record'], function (record) {
+    return {
+        pageInit : function(context) {
+            var salesOder = context.currentRecord;
+            
+        }
+    }
+});
+
+
+
+/*
+    Sublist Methods
+    Record.getLineCount()
+    Record.getSublistValue()
+    Record.setSublistValue()
+    Record.getSublistText()
+    Record.setSublistText()
+    Record.getCurrentSublistValue()
+    Recrod.setCurrentSublistValue()
+    Record.getCurrentSublistText()
+    Recrod.setCurrentSublistText()
+*/
