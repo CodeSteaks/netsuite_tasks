38af963995e2391c41744304e234451863841549
CodeSteaks <mianmujtaba89@gmail.com>
2024-03-07 10:05:22 +0500

    sublist entry points

--- src/FileCabinet/SuiteScripts/cs_sl.js
+++ src/FileCabinet/SuiteScripts/cs_sl.js
@@ -5,13 +5,16 @@
 
 define(['N/record'], function (record) {
     return {
-        pageInit : function(context) {
+        lineInit : function(context) {
             var salesOder = context.currentRecord;
             var item = record.getSublistValue({
                 group : 'item',
                 name  : 'amount',
                 line  : '2'
             });
+        },
+        validateLine : function(context) {
+            
         }
     }
 });
@@ -29,4 +32,14 @@ define(['N/record'], function (record) {
     Recrod.setCurrentSublistValue()
     Record.getCurrentSublistText()
     Recrod.setCurrentSublistText()
+*/
+
+
+/*
+    Sublist  Client Side Entry Points
+    User Action: Initialize a sublist line
+    Client Entry Point: lineInit
+
+    User Action: Validate a sublist line
+    Client Entry Point: validateLine
 */
